4846817a6b19585e6d978f3f253fc9e53a928787
Paul Lassa <plassa3@gmail.com>
2015-03-29 23:55:12 -0700

    Adding FilterModel - filter search params
    filter parameters to be passed in data structure

---
+++ app/src/main/java/com/ich/plassa/googleimagesearcher/FilterModel.java
@@ -0,0 +1,13 @@
+package com.ich.plassa.googleimagesearcher;
+
+import java.io.Serializable;
+
+/**
+ * Created by plassa on 3/29/15.
+ */
+public class FilterModel implements Serializable {
+        public String size = "small";
+        public String color;  // not used yet
+        public String type;  // not used yet
+        public String site;  // not used yet
+}
